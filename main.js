31f8363ed9aa2dd9487752971c71fea382a662a4
Khakim <59410504+khakim-tursunov@users.noreply.github.com>
2025-07-09 13:44:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var note_area = document.querySelector('.notearea');
+
+note_area.addEventListener('input', (e) => {
+    var note_area_value = note_area.value;
+
+    localStorage.setItem('note', note_area_value);
+    
+});
+
+var get_note = localStorage.getItem('note');
+note_area.value = get_note;
+
+//-------------------- Date Time -----------------------//
+var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const addZero = (i) => {
+    if (i < 10) {i = "0" + i}
+    return i;
+};
+setInterval(() => {
+    const date = new Date();
+    const day = weekday[date.getDay()];
+    const dayMonth = date.getDate();
+    const hour = addZero(date.getHours());
+    const minuts = addZero(date.getMinutes());
+    const seconds = addZero(date.getSeconds())
+
+    const dateTime = day + ' ' + dayMonth + ', ' + hour + ':' + minuts + ':' + seconds;
+    
+    document.querySelector('.note_block-top-time').innerHTML = dateTime;
+}, 1000);
+
+
+
+//------------------------------------------------//
+
+
+
+
