5d2b42fe531161e891d7d61b9d239f7bce4885e1
Khakim <59410504+khakim-tursunov@users.noreply.github.com>
2026-07-02 13:30:56 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ note_area.addEventListener('input', (e) => {
     var note_area_value = note_area.value;
 
     localStorage.setItem('note', note_area_value);
-    
+
 });
 
 var get_note = localStorage.getItem('note');
 note_area.value = get_note;
 
 //-------------------- Date Time -----------------------//
-// var weekday = ["Воскресение","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
+var weekday = ["Воскресение", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 
-// const addZero = (i) => {
-//     if (i < 10) {i = "0" + i}
-//     return i;
-// };
-// setInterval(() => {
-//     const date = new Date();
-//     const day = weekday[date.getDay()];
-//     const dayMonth = date.getDate();
-//     const hour = addZero(date.getHours());
-//     const minuts = addZero(date.getMinutes());
-//     const seconds = addZero(date.getSeconds())
+const addZero = (i) => {
+    if (i < 10) { i = "0" + i }
+    return i;
+};
+setInterval(() => {
+    const date = new Date();
+    const day = weekday[date.getDay()];
+    const dayMonth = date.getDate();
+    const hour = addZero(date.getHours());
+    const minuts = addZero(date.getMinutes());
+    const seconds = addZero(date.getSeconds())
 
-//     const dateTime = day + ' ' + dayMonth + ', ' + hour + ':' + minuts;
-    
-//     document.querySelector('.note_block-top-time').innerHTML = dateTime;
-// }, 1000);
+    const dateTime = day + ' ' + dayMonth + ', ' + hour + ':' + minuts;
+
+    document.querySelector('.note_block-top-time').innerHTML = dateTime;
+}, 1000);
 
 
 
